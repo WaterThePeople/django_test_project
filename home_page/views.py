19479c93607d9e562a357ed9260d1a12761f9b1a
Water The People <66826331+WaterThePeople@users.noreply.github.com>
2023-01-03 23:13:56 +0100

    update #1

---
+++ home_page/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.views.generic import ListView
+from django.views.generic import DetailView,CreateView,UpdateView,DeleteView
+from .models import Post,Comment
+from .forms import CommentForm
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request,'home_page/home.html',context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'home_page/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url = '/'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class AddCommentView(LoginRequiredMixin,CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'home_page/add_comment.html'
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        form.instance.post_id = self.kwargs['pk']
+        return super().form_valid(form)
+
+    success_url = '/'
+
+
+def welcome(request):
+    return render(request,'home_page/welcome.html',{'title':'Welcome'})
